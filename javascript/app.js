98358add680f48781e77938399db298a4b944921
kimmybeee <kimariehavertybozman@gmail.com>
2018-01-28 14:55:25 -0800

    TV shows as buttons with assigned data-value 'name'

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,35 @@
-//First set of buttons of my fave TV
-var tvShows =["Adventure Time", "Mindhunter", "Game of Thrones", "The Wire", "Empire"];
+//Array for first set of buttons of my fave TV
+var tvShows =["Adventure Time", "Mindhunter", "Empire", "Game of Thrones", "The Wire"];
 
-function 
+function makeButtons()	{
+//Dump made buttons out of the div to avoid repeat buttons when called upon
+	$("#tvshow-buttons").empty();
+
+//Loop through the array of tv shows
+	for (var i = 0; i < tvShows.length; i++)	{
+//Create a button
+		var newButton = $("<button>");
+//Add a class
+		newButton.addClass("tvshow");
+//Add a data-value "name" with index number of where it is in the array
+		newButton.attr("data-name", tvShows[i]);
+//Grabbing the value of "name" for the tvshow and using that value for the text on the button
+		newButton.text(tvShows[i]);
+//Add the named button to html
+		$("#tvshow-buttons").append(newButton);
+	}
+}
+
+//Make the buttons functional with a click function
+$("#find-tvshow").on("click", function(event)	{
+	
+	event.preventDefault();
+	
+	var tvShow = $("#tvshow-input").val().trim();
+	
+	makeButtons();
+
+	var queryURL
+});
+
+makeButtons();
